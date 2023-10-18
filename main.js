b181cf7fc04066a800bc9735fde6030f40df3106
Cindy Nguyen <CindyNguyen@computingforall.org>
2023-10-17 17:46:34 -0700

    practice session done

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 1. In index.html, link to this file using the script tag.
 2. Log the document in the console.
 */
+console.log(document);
 
 
 /* Exercise 2
@@ -13,8 +14,11 @@ Set its value to the method querySelector. Pass the method the id of the h1 tag.
 4. Log pageTitle2 in the console
 */
 
+let pageTitle = document.getElementById('page-title');
+console.log(pageTitle);
 
-
+let pageTitle2 = document.querySelector('#page-title');
+console.log(pageTitle2);
 
 /* Exercise 3
 1. Log the textContent of the pageTitle in the console
@@ -25,7 +29,12 @@ Give it a value of a string, pick one of the facts from moon-facts.txt
 5. Set its textContent to fact1
 */
 
+console.log(pageTitle.textContent);
+pageTitle.textContent = "Moon Facts";
 
+let fact1 = "Although it appears bright in the night sky, the Moon's surface is actually dark, with a reflectance just slightly higher than that of worn asphalt."
+let pin1 = document.querySelector('#moon-fact-1');
+pin1.textContent = fact1;
 
 
 
@@ -37,8 +46,21 @@ Hint - remember to escape the quote marks inside the p tag.
 4. Do the same thing for moon-fact-3, moon-fact-4, moon-fact-5
 */
 
+let fact2 = "<p class=fact> The Moon is drifting away from the Earth. It is moving approximately 3.8 cm away from our planet every year.</p>"
+let pin2 = document.querySelector('moon-fact-2');
+pin2.textContent =+ fact2;
 
+let fact3 = "<p class=fact>The Moon always shows Earth the same face, it is in synchronous rotation with Earth.</p>"
+let pin3 = document.querySelector('#moon-fact-3');
+pin3.innerHTML += fact3;
 
+let fact4 = "<p class=fact>The Moon is the second densest satellite, among those whose densities are known. The first densest is Jupiter's satellite Io.</p>"
+let pin4 = document.querySelector("#moon-fact-4");
+pin4.innerHTML += fact4;
+
+let fact5 = "<p class=fact>There is water on the Moon! It has been detected on areas of the lunar surface that are in permanent shadow and was likely delivered to the surface by comets.</p>"
+let pin5 = document.querySelector("#moon-fact-5");
+pin4.innerHTML += fact5;
 
 /* Exercise 5
 1. Use the DOM to select the image tag with id "secret"
@@ -46,3 +68,12 @@ Hint - remember to escape the quote marks inside the p tag.
 3. Select the div tag and add a p tag to its innerHTML 
 Add some text inside the p tag about a secret alien moon base.
 */
+
+let pinImage = document.querySelector('#selector');
+pinImage.src = 'alien-ship.png';
+
+let fact6 = "<p class=fact>The Moon has quakes called moonquakes. They are caused by the gravitational influence of the Earth and can last up to half an hour.</p>"
+let pin6 = document.querySelector("#moon-fact-6");
+pin6.innerHTML += fact6;
+
+console.log(pinImage);
--- practice-session/practice-session.js
+++ practice-session/practice-session.js
@@ -14,3 +14,28 @@ Hint 2 - Add a unigue id to each tag where you need to add JavaScript
 Hint 3 - Don't forget to use the script tag to link to this file.
 Hint 4 - Use variables to save yourself some repetitive typing.
 */
+
+
+let comment1 = document.querySelector('#comment1');
+comment1.textContent = "That is the silliest thing I've ever heard!";
+console.log(comment1);
+
+let comment2 = document.querySelector('#comment2');
+comment2.textContent = 'That is true, I searched it up.';
+console.log(comment2);
+
+let comment3 = document.querySelector('#comment3');
+comment3.textContent = 'That is true, I searched it up.';
+console.log(comment3);
+
+let comment4 = document.querySelector('#comment4');
+comment4.textContent = 'That is not true at all.';
+console.log(comment4);
+
+let comment5 = document.querySelector('#comment5');
+comment5.textContent = 'That is true, I searched it up';
+console.log(comment5);
+
+let comment6 = document.querySelector('#comment6');
+comment6.textContent = 'That is true, I searched it up.';
+console.log(comment6);
